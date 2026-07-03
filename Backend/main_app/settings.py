0fa498eb5a6c31368695d0a7d2eff5c0c6abb75c
Sebastian L <jleguizamons@unal.edu.co>
2026-07-03 15:33:47 -0500

    feat(auth): complete authentication module

--- Backend/main_app/settings.py
+++ Backend/main_app/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.postgres",
     "apps.authentication",
     "rest_framework",
-    "rest_framework_simplejwt.token_blacklist"
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 """
